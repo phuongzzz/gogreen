c9f15ae2d7e1aacac224acb883aeec940b8e23c2
phuongzzz <yesterdayoncemore23@gmail.com>
2018-03-14 22:18:02 +0700

    Create Main Component

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -12,21 +6,13 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import Main from './src/main';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to GoGreen!
-        </Text>
+        <Main/>
       </View>
     );
   }
@@ -38,15 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#23d6aa',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello, Go Green!
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Main
+
